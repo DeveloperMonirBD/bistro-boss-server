3f5c1db0ee229242ee76d90df935010b105c9a4d
Monir <mrmonir0558@gmail.com>
2025-01-03 12:10:22 +0600

    added delete method in the cartCollection

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhc73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -59,6 +59,15 @@ async function run() {
             const result = await cartCollection.insertOne(cartItem);
             res.send(result)
         })
+
+        // delete cart in db
+        app.delete('/carts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+        })
+
         
 
         // Send a ping to confirm a successful connection
